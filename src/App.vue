0a6ad333837ae8a2e73c90aeaf1749023538c145
vklivak <vklivak@air.io>
2022-11-24 18:33:53 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 pt-5 pb-5">
+          <b-card no-body >
+            <b-tabs pills card vertical class="tabs-list">
+              <!-- Lab 1 -->
+              <b-tab title="LAB #1" active>
+                <b-card-text>Lab 1 content</b-card-text>
+              </b-tab>
+
+              <!-- Lab 2 -->
+              <b-tab title="LAB #2">
+                <b-card-text>Lab 2 content</b-card-text>
+              </b-tab>
+            </b-tabs>
+          </b-card>
+        </div>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.tabs-list {
+  min-height: 500px;
+}
+
+.tabs-list li {
+  margin-bottom: 5px;
 }
 </style>
